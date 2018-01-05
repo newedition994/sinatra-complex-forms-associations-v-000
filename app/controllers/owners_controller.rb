e5f0a4efbf11695051b27086db08c09991aa7123
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-05 05:11:35 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -18,6 +18,7 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
+    @owner.save
   end
 
   get '/owners/:id/edit' do
